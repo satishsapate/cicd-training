c11ddf7537ca12bbc9705e8a6cf491b4361e76f4
Satish Sapate <satish.sapate@zensar.com>
2019-04-29 15:03:31 +0530

    file changes

--- Training.java
+++ Training.java
@@ -1,7 +1,7 @@
 class Training {
  public static void main(String args[]){
  
-     System.out.println("Hello World!!!!!!");
+     System.out.println("CICD Training......Hello World!!!!!!");
 	 
  }
 }
